00f51f481559348a8545b9cf1f02a582dca99085
fuster92 <fusti92@gmail.com>
2023-06-28 23:31:08 +0200

    feat(chat): add chat API request and response

--- chat.go
+++ chat.go
@@ -1,1 +1,31 @@
 package openai
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type ChatRequest struct {
+	Model       string    `json:"model"`
+	Messages    []Message `json:"messages"`
+	Temperature float64   `json:"temperature,omitempty"`
+}
+
+type Usage struct {
+	PromptTokens     int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens      int `json:"total_tokens"`
+}
+
+type ChatResponse struct {
+	Id      string `json:"id"`
+	Object  string `json:"object"`
+	Created int64  `json:"created"`
+	Model   string `json:"model"`
+	Usage   Usage  `json:"usage"`
+	Choices []struct {
+		Message      Message `json:"message"`
+		Index        int     `json:"index"`
+		FinishReason string  `json:"finish_reason"`
+	}
+}
