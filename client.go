8ddac4e4d24f51ccdf11f4c651e20141c7677e3a
fuster92 <fusti92@gmail.com>
2023-06-28 23:34:53 +0200

    feat(chat): add openai client

---
+++ client.go
@@ -0,0 +1,58 @@
+package openai
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/philippta/trip"
+	"net/http"
+	"time"
+)
+
+const baseUrl = "https://api.openai.com"
+
+type Client struct {
+	httpClient *http.Client
+	baseUrl    string
+}
+
+func (c *Client) Chat(r *ChatRequest) (*ChatResponse, error) {
+	chatEndpoint := "chat/completions"
+	data, err := json.Marshal(&r)
+	if err != nil {
+		return nil, fmt.Errorf("error sending chat request: %v", err)
+	}
+	resp, err := c.httpClient.Post(
+		fmt.Sprintf("%s/%s", c.baseUrl, chatEndpoint),
+		"application/json",
+		bytes.NewBuffer(data),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error reading chat response: %v", err)
+	}
+	defer resp.Body.Close()
+	response := ChatResponse{}
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding chat response: %v", err)
+	}
+	return &response, nil
+}
+
+func NewClient(token, version string) *Client {
+	var (
+		attempts = 3
+		delay    = 50 * time.Millisecond
+	)
+
+	client := &http.Client{
+		Transport: trip.Default(
+			trip.BearerToken(token),
+			trip.Retry(attempts, delay, trip.RetryableStatusCodes...),
+		),
+	}
+	return &Client{
+		httpClient: client,
+		baseUrl:    fmt.Sprintf("%s/%s", baseUrl, version),
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fuster92/openai
 
 go 1.20
+
+require github.com/philippta/trip v0.2.0
